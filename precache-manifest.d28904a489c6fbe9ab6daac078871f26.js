cb0ffece099d36612ae7fc1f85710a867940614d
Harsh-2420 <harsh2420@gmail.com>
2020-02-19 02:58:03 -0500

    Updates

---
+++ precache-manifest.d28904a489c6fbe9ab6daac078871f26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8daaa861a82a7b718531a9be82f7e7b4",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "a80086d36b2a3c49da64",
+    "url": "/tictactoe/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4b48eb8e720b12554cc4",
+    "url": "/tictactoe/static/js/2.a9afe336.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tictactoe/static/js/2.a9afe336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a80086d36b2a3c49da64",
+    "url": "/tictactoe/static/js/main.a8eb27ef.chunk.js"
+  },
+  {
+    "revision": "f9796d282907b93526d6",
+    "url": "/tictactoe/static/js/runtime-main.e11d0aff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictactoe/static/media/logo.5d5d9eef.svg"
+  }
+]);
